a3636da94bbd9ab081bc49547ad41dc8a06656ac
charmruber <1912758611@qq.com>
2015-07-18 04:02:19 +0800

    steping...

--- readpuzzle.js
+++ readpuzzle.js
@@ -4,8 +4,27 @@ var fs = require('fs');
 var pz = require('./puzzle.json');
 
 var context = pz.easy[0].context;
-console.log(context);
+var shudu = {};
 
-for(var i = 0; i < console.length; i++) {
-
+// heng
+for(var i = 0; i < 9; i++) {
+    shudu['h' + (i + 1)] = context[i];
 }
+
+// zong
+for(var i = 0; i < 9; i++) {
+    shudu['z' + (i + 1)] = [];
+}
+for(var i = 0; i < 9; i++) {    // h
+    for(var j = 0; j < 9; j++) {   // z
+        shudu['z' + (j + 1)][i] = context[i][j];
+    }
+}
+
+// gongge
+for(var i = 0; i < 9; i++) {
+    shudu['g' + (i + 1)] = [];
+}
+for
+
+console.error(shudu);
